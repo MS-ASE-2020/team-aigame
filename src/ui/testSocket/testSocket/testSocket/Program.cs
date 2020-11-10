778eb9282803ed955860e02f08a8fd36aef492c4
Yue Zhang <v-yuez1@microsoft.com>
2020-11-10 23:12:24 +0800

    test program

---
+++ src/ui/testSocket/testSocket/testSocket/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+
+namespace testSocket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Dictionary<string, Socket> dickSocket = new Dictionary<string, Socket>();
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipe = new IPEndPoint(ip, 6006);
+            socket.Bind(ipe);
+            socket.Listen(2);
+        }
+    }
+}
